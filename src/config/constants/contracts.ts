3d9d1cb2099dc20baccf5112286aa295e4ac2251
swebexport <iwan930409@outlook.com>
2021-03-11 01:08:29 +0900

    create repository

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,32 @@
+export default {
+  cake: {
+    // 56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0xD21c5c947ca56d3947851e4F1Cd0164849f50fE0',
+    97: '',
+  },
+  masterChef: {
+    // 56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0xb02f2D84aaC20584cdfD49f29D3DE06286069533',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
